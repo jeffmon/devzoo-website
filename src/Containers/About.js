7762255358a2cb687671551e14bcb27a52354776
jeffmon <jeffmon@gmail.com>
2018-01-10 21:44:26 -1000

    forgot changes idk lol

--- src/Containers/About.js
+++ src/Containers/About.js
@@ -4,7 +4,12 @@ class About extends Component {
   render() {
     return (
       <div className="about_us_container">
-        <div className="about_us_content">About Us Content</div>
+        <div className="about_us_content">
+          About Us: Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Eum velit architecto distinctio perspiciatis ad debitis earum.
+          Incidunt voluptatum suscipit exercitationem et cumque, nostrum vitae
+          deserunt hic similique fugit explicabo. Dignissimos!
+        </div>
         <div className="about_us_img">About Us IMG</div>
       </div>
     );
